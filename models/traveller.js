c02f465df41e35795bf1e7ab2d4f647488e4ee1d
Cameron Wilson <cameronwilson@Camerons-MacBook-Air.local>
2022-06-08 21:09:01 +0100

    first commit

---
+++ models/traveller.js
@@ -0,0 +1,47 @@
+const Journey = require("./journey");
+
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return this.journeys.transport === transport;
+  });
+  return journey
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  const result = this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+});
+return result;
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  let total = this.journeys.reduce((distance_total, journey) => {
+    return distance_total + journey.distance;
+},0);
+return total;
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const tarnsport = this.journeys.find((journey) => {
+
+  })
+};
+
+
+module.exports = Traveller;
